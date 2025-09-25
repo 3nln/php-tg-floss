682e2688fbf27c9c2557b89e4d7ca0d21a06d016
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2025-09-25 23:42:16 +0500

    Registering existing bot commands

--- src/app/Commands/StartCommand.php
+++ src/app/Commands/StartCommand.php
@@ -3,18 +3,21 @@
 namespace App\Commands;
 
 use App\Buttons\InlineButtons\CommandInlineButtons;
+use App\Utils\Traits\GeneralTrait;
 use SergiX44\Nutgram\Handlers\Type\Command;
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Telegram\Properties\ParseMode;
+use SergiX44\Nutgram\Telegram\Types\Command\BotCommandScopeAllPrivateChats;
 
 class StartCommand extends Command
 {
-    use CommandInlineButtons;
+    use CommandInlineButtons, GeneralTrait;
     protected string $command = 'start';
 
     protected string $message = "Assalomu alaykum! Botga xush kelibsiz. \n\nUshbu bot *PHP Uzbekistan* jamiyati tomonidan yaratilgan bot hisoblanib, O'zbek PHP jamiyati uchun foydali bo'lgan resurslarni yetkazish, saqlash va saralash uchun xizmat qiladi.";
     public function handle(Nutgram $bot): void
     {
+        $bot->setMyCommands($this->botCommands(), new BotCommandScopeAllPrivateChats(), 'uz');
         $bot->sendMessage(
             text: $this->message,
             parse_mode: ParseMode::MARKDOWN_LEGACY,
--- src/app/Utils/Traits/GeneralTrait.php
+++ src/app/Utils/Traits/GeneralTrait.php
@@ -3,6 +3,7 @@
 namespace App\Utils\Traits;
 
 use SergiX44\Nutgram\Nutgram;
+use SergiX44\Nutgram\Telegram\Types\Command\BotCommand;
 
 trait GeneralTrait
 {
@@ -11,5 +12,17 @@ trait GeneralTrait
         $data = $bot->callbackQuery()?->data ?? $bot->message()?->text ?? "something_else";
         $back_signs = ["back", "go_back"];
         return in_array($data, $back_signs);
+    }
+
+    public function botCommands(): array
+    {
+        return [
+            BotCommand::make('start', 'Botni qayta ishga tushirish'),
+            BotCommand::make('rules', 'Jamiyatimiz qoidalari'),
+            BotCommand::make('about', 'Ushbu botimizni rivojlantirish haqida'),
+            BotCommand::make('roadmap', "Boshlang'ich PHP yo'l xaritasi"),
+            BotCommand::make('group', 'PHPga oid guruhlar va hamjamiyatlar'),
+            BotCommand::make('useful', "Foydali resurslar ro'yxati"),
+        ];
     }
 }
